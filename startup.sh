ade9fb337240b4a88ac019e5c0863ce2c2a67056
Ryan Luker <ryanluker@outlook.com>
2017-01-10 10:32:54 -0800

    cleanup vagrantfile and startup to use sync folder

--- startup.sh
+++ startup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+echo "############Setup folders for vagrant syncing"
+sudo rm -rf ../service_fabric_data
+sudo mkdir -p ../service_fabric_data
+echo "############Setup folders for vagrant syncing - DONE"
+
 vagrant up
 
 echo "############Linking java dependencies to local dev environment"
 sudo rm -rf /opt/microsoft/sdk/servicefabric/java/packages/lib/
 sudo mkdir -p /opt/microsoft/sdk/servicefabric/java/packages/lib/
 sudo cp -r tmp/lib/ /opt/microsoft/sdk/servicefabric/java/packages/lib/
+echo "############Linking java dependencies to local dev environment - DONE"
